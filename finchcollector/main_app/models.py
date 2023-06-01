e99d8517438582176327459d0e41e4406aa0128a
Mrs_N <worksmart377@gmail.com>
2023-06-01 18:29:52 -0500

    adds more routes and functionality

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    home = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    fact = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"finch_id": self.id})
