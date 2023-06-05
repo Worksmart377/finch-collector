28c02b56cc6bfe8e394e439746f366f11e8a3980
Mrs_N <worksmart377@gmail.com>
2023-06-04 19:43:13 -0500

    completes all parts

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from .models import Finch, Toy
+from django.shortcuts import render, redirect
 from .models import Finch
+from .forms import FeedingForm
 
 
-finches = [
-    {'name': 'American Goldfinch', 'home': 'North America & Mexico', 'description': 'tiny migratory bird', 'fact': 'one of the strictest vegetarian birds in the world'},
-    {'name': 'Brambling or Mountain finch', 'home': 'Europe & Africa & Alaska', 'description': 'international migratory bird', 'fact': 'they fly in flocks, and millions gather for migration'},
-    {'name': 'Evening Grosbeak', 'home': 'Southern California', 'description': 'north american bulky looking finch', 'fact': 'they have powerful beaks that can crush seeds that other birds can not'},
-    {'name': 'Hawfinch', 'home': 'Europe & Asia', 'description': 'short stocky finch with large bills', 'fact': 'the only finch placed in the genus coccothraustes'},
-    {'name': 'Pine Grosbeak', 'home': 'Alaska', 'description': 'adorable qual looking finch', 'fact': 'their song depends on their geographical range'},
-    {'name': 'Eurasian Bullfinch', 'home': 'Europe & Asia', 'description': 'worm-eating bird', 'fact': 'capable of learning specific melodies'},
-    {'name': 'Purple Finch', 'home': 'North America', 'description': 'sparrow dipped in rasberry juice', 'fact': 'they are masters at breaking seeds with their beaks', 'image':'../static/imgs/purplefinch.jpg'},
-]
 # Create your views here.
 def home(request):
     # Include an .html file extension - unlike when rendering EJS templates
@@ -28,8 +23,41 @@ def finches_index(request):
     
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', { 'finch': finch })
+    id_list = finch.toys.all().values_list('id') # [1, 3, 7]
 
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', 
+    { 
+        'finch': finch,
+        'feeding_form': feeding_form,
+        'toys': toys_finch_doesnt_have
+    })
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+
+    # validate the form
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        '''
+        {
+            meal: 'B',
+            date: '2023-06-02',
+            cat_id: 1
+        }
+        '''
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
 class FinchCreate(CreateView):
     model = Finch
     fields = ['name', 'home', 'description', 'fact']
@@ -42,3 +70,21 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches/'
+    
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
